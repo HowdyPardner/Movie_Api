2083f28c13bf6e70c0a00b27078317e47b3aee6b
HowdyPardner <msb.misc@gmail.com>
2023-09-24 22:16:14 -0700

    Add user input form for data search criteria

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 
 const Form = () => {
-  return (
-    <div>Form</div>
-  )
+    const [formData, setFormData] = useState({
+        searchterm: "",
+    });
+
+    const handleChange = (event) => {
+        setFormData({ ...formData, [event.target.name]: event.target.value });
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        props.moviesearch(formData.searchterm);
+    };
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    name="searchterm"
+                    onChange={handleChange}
+                    value={formData.searchterm}
+                />
+                <input type="submit" value="submit" />
+            </form>
+        </div>
+    );
 }
 
 export default Form
